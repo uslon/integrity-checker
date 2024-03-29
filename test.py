2f0d139ea6a34b73435a35c5b71c40bce4aa0407
uslon <uslonp18@yandex.ru>
2024-03-29 10:05:00 +0300

    graceful cleanup

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import os
 import signal
 import subprocess
 import time
+import unittest
 
 
 def get_last_syslog():
@@ -11,67 +12,68 @@ def get_last_syslog():
     last_entry = subprocess.check_output(("tail", "-n", "1"), stdin=grep.stdout).decode("utf-8")
     return last_entry
 
-def main():
-    subprocess.run(['make'])
+def do_cleanup(pid):
+    os.kill(pid, signal.SIGTERM)
+    os.waitpid(pid, 0)
 
-    text_path = "text.txt"
-    with open(text_path, "w+") as text_file:
-        text_file.write("Test text.");
+class Testing(unittest.TestCase):
+    def test_all(self):
+        subprocess.run(['make'])
 
-    pwd = os.getcwd()
-    binary_path = os.path.join(pwd, "integrity_checker")
-    daemon = subprocess.Popen([binary_path, "-time_interval", "2", "-directory", pwd])
+        text_path = "text.txt"
+        with open(text_path, "w+") as text_file:
+            text_file.write("Test text.");
 
-    time.sleep(2.2)
+        pwd = os.getcwd()
+        binary_path = os.path.join(pwd, "integrity_checker")
+        daemon = subprocess.Popen([binary_path, "-time_interval", "2", "-directory", pwd])
+        self.addClassCleanup(do_cleanup, daemon.pid)
 
-    last_entry = get_last_syslog().split()
-    binary_in_log = binary_path + "[" + str(daemon.pid) + "]:"
-    assert (binary_in_log in last_entry)
-    assert ("OK" == last_entry[-1][:2])
+        time.sleep(2.2)
 
-
-    with open(text_path, "w+") as text_file:
-        text_file.write("Another test text.")
-    os.kill(daemon.pid, signal.SIGUSR1)
-    time.sleep(0.5)
-
-    last_entry = get_last_syslog().split()
-    assert ("FAIL" in last_entry)
-    assert (binary_in_log in last_entry)
-    assert ("check" in last_entry)
-    assert ("sums" in last_entry)
-    assert ("differ:" in last_entry)
+        last_entry = get_last_syslog().split()
+        binary_in_log = binary_path + "[" + str(daemon.pid) + "]:"
+        self.assertTrue(binary_in_log in last_entry)
+        self.assertTrue("OK" == last_entry[-1][:2])
 
 
-    os.chmod(text_path, 000)
-    os.kill(daemon.pid, signal.SIGUSR1)
-    time.sleep(0.5)
+        with open(text_path, "w+") as text_file:
+            text_file.write("Another test text.")
+        os.kill(daemon.pid, signal.SIGUSR1)
+        time.sleep(0.5)
 
-    last_entry = get_last_syslog().split()
-    assert ("FAIL" in last_entry)
-    assert (binary_in_log in last_entry)
-    assert ("file" in last_entry)
-    assert ("is" in last_entry)
-    assert ("NOT_ACCESSIBLE)" in last_entry)
-
-
-    os.remove(text_path)
-    os.kill(daemon.pid, signal.SIGUSR1)
-    time.sleep(0.5)
-
-    last_entry = get_last_syslog().split()
-    assert ("FAIL" in last_entry)
-    assert (binary_in_log in last_entry)
-    assert ("file" in last_entry)
-    assert ("is" in last_entry)
-    assert ("NOT_FOUND)" in last_entry)
+        last_entry = get_last_syslog().split()
+        self.assertTrue("FAIL" in last_entry)
+        self.assertTrue(binary_in_log in last_entry)
+        self.assertTrue("check" in last_entry)
+        self.assertTrue("sums" in last_entry)
+        self.assertTrue("differ:" in last_entry)
 
 
-    os.kill(daemon.pid, signal.SIGTERM)
-    os.waitpid(daemon.pid, 0)
+        os.chmod(text_path, 000)
+        os.kill(daemon.pid, signal.SIGUSR1)
+        time.sleep(0.5)
 
-    print("test is OK!")
+        last_entry = get_last_syslog().split()
+        self.assertTrue("FAIL" in last_entry)
+        self.assertTrue(binary_in_log in last_entry)
+        self.assertTrue("file" in last_entry)
+        self.assertTrue("is" in last_entry)
+        self.assertTrue("NOT_ACCESSIBLE)" in last_entry)
+
+
+        os.remove(text_path)
+        os.kill(daemon.pid, signal.SIGUSR1)
+        time.sleep(0.5)
+
+        last_entry = get_last_syslog().split()
+        self.assertTrue("FAIL" in last_entry)
+        self.assertTrue(binary_in_log in last_entry)
+        self.assertTrue("file" in last_entry)
+        self.assertTrue("is" in last_entry)
+        self.assertTrue("NOT_FOUND)" in last_entry)
 
 
 if __name__ == '__main__':
-    main()
+
+    unittest.main()
